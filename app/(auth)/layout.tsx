2b057de26ef0e38e2f4601f7f5bdd07b3c39ecc4
leoanrdo <programacao@studioalpha.com.br>
2024-05-07 13:25:24 -0300

    feat: create authentication with clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AuthLayout(
+    {children}: {children: React.ReactNode}
+) {
+  return (
+    <div className='flex w-full h-screen justify-center items-center'>
+        {children}
+    </div>
+  )
+}
